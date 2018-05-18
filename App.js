6b6996a8a5f949c948ce12c54e10a7a181d86109
lywoo-sroeng <lywoo.sroeng@gmail.com>
2018-05-18 23:14:44 +0700

    flexbox

--- App.js
+++ App.js
@@ -9,9 +9,20 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Hello World!
-        </Text>
+
+        <View style={styles.section1}>
+          <View style={styles.section1_1}/>
+          <View style={styles.section1_2}/>
+        </View>
+
+        <View style={styles.section2}>
+          <View style={styles.section2_1}/>
+          <View style={styles.section2_2}>
+            <View style={styles.section2_2_1}/>
+            <View style={styles.section2_2_2}/>
+          </View>
+        </View>
+
       </View>
     );
   }
@@ -20,8 +31,45 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    margin: 5,
+    marginTop: 20
+  },
+  section1: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  section1_1: {
+    flex: 1,
+    backgroundColor: 'black',
+    margin: 5
+  },
+  section1_2: {
+    flex: 0.3,
+    backgroundColor: 'green',
+    margin: 5
+  },
+  section2: {
+    flex: 1.5,
+    flexDirection: 'row'
+  },
+  section2_1: {
+    flex: 1,
+    backgroundColor: 'green',
+    margin: 5
+  },
+  section2_2: {
+    flex: 1.2,
+    flexDirection: 'column'
+  },
+  section2_2_1: {
+    flex: 1,
+    backgroundColor: 'grey',
+    margin: 5
+  },
+  section2_2_2: {
+    flex: 1,
+    backgroundColor: 'black',
+    margin: 5
   }
 });
